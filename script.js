0699ba4bbf774c7be5464128c290cf659c06a41a
KatlynGreen <batgalkgg@gmail.com>
2022-10-10 06:33:45 -0400

    completed API callback and deconstructed objects to display the required information for the dashboard

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 let weather = {
   apiKey: "6391db290f4bb2b64fb92fad43316f21",
-  fetchWeather: function () {
+  fetchWeather: function (city) {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
         "&units=metric&appid=" +
-        apiKey
+        this.apiKey
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => this.displayWeather(data));
   },
-  displayWeather: function (data) {},
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind Speed: " + speed + " km/hr";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".searchBar").value);
+  },
 };
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+document
+  .querySelector(".searchBar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
